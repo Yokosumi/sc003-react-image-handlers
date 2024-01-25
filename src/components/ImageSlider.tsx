9271f631fd68f5fa186252e256e49215df304b61
Selman Ciftci <selman.ciftci.coding@gmail.com>
2024-01-25 12:15:29 +0100

    adds prev and next image functions

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -8,6 +8,20 @@ type ImageSliderProps = {
 export const ImageSlider = ({ imageUrls }: ImageSliderProps) => {
     const [imageIndex, setImageIndex] = useState(0)
 
+    const showNextImage = () => {
+        setImageIndex((index) => {
+            if (index === imageUrls.length - 1) return 0
+            return index + 1
+        })
+    }
+
+    const showPrevImage = () => {
+        setImageIndex((index) => {
+            if (index === 0) return imageUrls.length - 1
+            return index - 1
+        })
+    }
+
     return (
         <div style={{ width: '100%', height: '100%', position: 'relative' }}>
             <img
@@ -15,10 +29,18 @@ export const ImageSlider = ({ imageUrls }: ImageSliderProps) => {
                 src={imageUrls[imageIndex]}
                 alt="cat"
             />
-            <button className="img-slider-btn" style={{ left: '0' }}>
+            <button
+                className="img-slider-btn"
+                style={{ left: '0' }}
+                onClick={showPrevImage}
+            >
                 <ArrowLeft />{' '}
             </button>
-            <button className="img-slider-btn" style={{ right: '0' }}>
+            <button
+                className="img-slider-btn"
+                style={{ right: '0' }}
+                onClick={showNextImage}
+            >
                 <ArrowRight />
             </button>
         </div>

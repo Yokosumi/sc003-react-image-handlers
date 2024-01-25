32deec045a2b0797e0d4c3006ef95bbd22b26a2f
Selman Ciftci <selman.ciftci.coding@gmail.com>
2024-01-25 11:01:28 +0100

    Update image URLs in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { ImageSlider } from './components/ImageSlider'
-import cat1 from '../../public/images/cat_1.jpg'
-import cat2 from '../../public/images/cat_2.jpg'
-import cat3 from '../../public/images/cat_3.jpg'
-import cat4 from '../../public/images/cat_4.jpg'
-import cat5 from '../../public/images/cat_5.jpg'
+import cat1 from '../public/images/cat_1.jpg'
+import cat2 from '../public/images/cat_2.jpg'
+import cat3 from '../public/images/cat_3.jpg'
+import cat4 from '../public/images/cat_4.jpg'
+import cat5 from '../public/images/cat_5.jpg'
 import './index.scss'
 
 const Images = [cat1, cat2, cat3, cat4, cat5]
@@ -11,7 +11,7 @@ const Images = [cat1, cat2, cat3, cat4, cat5]
 export const App = () => {
     return (
         <>
-            <ImageSlider imageUrls={image} />
+            <ImageSlider imageUrls={Images} />
         </>
     )
 }
